c34ebe14cb1f01e9839da4f5eeea9155315d8767
A-Elbana <manelbana094@gmail.com>
2025-12-07 13:54:31 +0200

    Added ControlUnit and LoadToPCFSM VHDL files with initial implementation

---
+++ ControlUnit.vhdl
@@ -0,0 +1,132 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ControlUnit is
+    port(
+        inst_bits : in std_logic_vector(4 downto 0);
+        EXMEM_MemOp, HWInt : in std_logic;
+        MemToReg, RegWrite, PCStore, MemOp_Inst, MemOp_Priority, MemRead : out std_logic;
+        MemWrite, RET, RTI, InputOp, ALUSrc, OutOp : out std_logic;
+        SWINT, JMPCALL, HLT, PCWrite, SWP, Imm32 : out std_logic;
+        IFID_EN, IDEX_EN, EXMEM_EN, MEMWB_EN : out std_logic;
+        StackOpType, ALUOPType, JMPType : out std_logic_vector(1 downto 0)
+
+    );
+end entity ControlUnit;
+
+architecture RTL of ControlUnit is
+    
+    signal decoded_inst : std_logic_vector(31 downto 0);
+    signal Imm : std_logic;
+    signal RETURNINST : std_logic;
+    
+begin
+
+    decoded_inst(to_integer(unsigned(inst_bits))) <= '1';
+
+    MemToReg <= decoded_inst(7) -- POP
+        or decoded_inst(19); -- LDD
+        
+    RegWrite <= decoded_inst(2) -- ALUOP
+        or decoded_inst(4) -- IN
+        or decoded_inst(7) -- POP
+        or decoded_inst(9) -- IADD
+        or decoded_inst(10) -- LDM
+        or decoded_inst(19) -- LDD
+        or decoded_inst(31); -- SWAP
+        
+    
+    PCStore <= decoded_inst(15) -- CALL
+        or decoded_inst(17); -- INT
+    
+    MemOp_Inst <= decoded_inst(7) -- POP
+        or decoded_inst(19) -- LDD
+        or decoded_inst(16) -- RET
+        or decoded_inst(18) -- RTI
+        or decoded_inst(6) -- PUSH
+        or decoded_inst(15) -- CALL
+        or decoded_inst(20); -- STD
+
+    MemRead <= decoded_inst(7) -- POP
+        or decoded_inst(19) -- LDD
+        or decoded_inst(16) -- RET
+        or decoded_inst(18); -- RTI
+
+    MemWrite <= decoded_inst(6) -- PUSH
+        or decoded_inst(15) -- CALL
+        or decoded_inst(20); -- STD
+
+    RETURNINST <= decoded_inst(16) -- RET
+        or decoded_inst(18); -- RTI
+    
+    RET <= RETURNINST;
+    
+    RTI <= decoded_inst(18); -- RTI
+
+    InputOp <= decoded_inst(4); -- IN
+
+    OutOp <= decoded_inst(3); -- OUT
+
+    ALUSrc <= decoded_inst(9) -- IADD
+    or decoded_inst(10) -- LDM
+    or decoded_inst(19) -- LDD
+    or decoded_inst(20); -- STD
+
+    SWINT <= decoded_inst(17); -- INT
+
+    JMPCALL <= decoded_inst(14) -- JMP
+        or decoded_inst(15); -- CALL
+    
+    HLT <= decoded_inst(1); -- HLT
+
+    SWP <= decoded_inst(31); -- SWAP
+
+    PCWrite <= decoded_inst(1) -- HLT
+    or decoded_inst(31) -- SWAP
+    or (EXMEM_MemOp and not Imm);
+    -- This is the Stack Operation Discriminator bit
+    StackOpType(0) <= decoded_inst(7) -- POP
+    or RETURNINST; -- Return Instructions
+    -- This is the Stack Enable bit
+    StackOpType(1) <= decoded_inst(15) -- CALL
+    or decoded_inst(17) -- INT
+    or decoded_inst(7) -- POP
+    or RETURNINST; -- Return Instructions
+
+    ALUOPType(0) <= decoded_inst(15) -- CALL
+    or decoded_inst(10) -- LDM
+    or decoded_inst(19) -- LDD
+    or decoded_inst(20) -- STD
+    or decoded_inst(2) -- ALUOP
+    or decoded_inst(9) -- IADD
+    or decoded_inst(6); -- PUSH
+
+    ALUOPType(1) <= decoded_inst(19) -- LDD
+    or decoded_inst(20) -- STD
+    or decoded_inst(2) -- ALUOP
+    or decoded_inst(9); -- IADD
+
+    JMPType(0) <= decoded_inst(11) -- JZ
+    or decoded_inst(13); -- JC
+
+    JMPType(1) <= decoded_inst(12) -- JN
+    or decoded_inst(13); -- JC
+
+    Imm <= decoded_inst(15) -- CALL
+    or decoded_inst(10) -- LDM
+    or decoded_inst(19) -- LDD
+    or decoded_inst(20) -- STD
+    or decoded_inst(11) -- JZ
+    or decoded_inst(12) -- JN
+    or decoded_inst(13) -- JC
+    or decoded_inst(14) -- JMP
+    or decoded_inst(9); -- IADD
+
+    Imm32 <= Imm;
+    MemOp_Priority <= (EXMEM_MemOp and not Imm) or HWInt;
+    IFID_EN <= Imm or (EXMEM_MemOp and not Imm) or RETURNINST;
+    IDEX_EN <= Imm;
+    EXMEM_EN <= Imm;
+    MEMWB_EN <= Imm;
+end architecture RTL;
---
+++ LoadToPCFSM.vhdl
@@ -0,0 +1,71 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity LoadToPCFSM is
+    port(
+        clk             : in  std_logic;
+        rst             : in  std_logic;
+        interrupt_index : in  std_logic_vector(2 downto 0);
+        memOp           : in  std_logic;
+        INT             : in  std_logic;
+        LoadPC          : out std_logic;
+        int_index       : out std_logic_vector(2 downto 0);
+        IFID_FLUSH         : out std_logic
+    );
+end entity LoadToPCFSM;
+
+architecture RTL of LoadToPCFSM is
+    type st is (IDLE, WAITING);
+
+    signal state_reg  : st := IDLE;
+    signal next_state : st := IDLE;
+
+begin
+
+    state_register : process(clk, rst) is
+    begin
+        if rst = '1' then
+            state_reg <= IDLE;
+        elsif rising_edge(clk) then
+            state_reg <= next_state;
+        end if;
+    end process state_register;
+
+    int_index_reg : process(clk, rst) is
+    begin
+        if rst = '1' then
+            int_index <= (others => '0');
+        elsif rising_edge(clk) then
+            if INT = '1' then
+                int_index <= interrupt_index;
+            end if;
+
+        end if;
+    end process int_index_reg;
+
+    next_state_logic : process(rst, memOp, INT, state_reg) is
+    begin
+        if rst = '1' then
+            LoadPC  <= '0';
+            IFID_FLUSH <= '0';
+
+        else
+            LoadPC  <= '0';
+            IFID_FLUSH <= '0';
+            case state_reg is
+                when IDLE =>
+                    if INT = '1' then
+                        next_state <= WAITING;
+                    end if;
+                when WAITING =>
+                    if memOp = '0' then
+                        next_state <= IDLE;
+                        LoadPC <= '1';
+                    end if;
+                    IFID_FLUSH <= '1';
+            end case;
+        end if;
+    end process next_state_logic;
+
+end architecture RTL;
